54ba37a50f0b0df01914e84b861019bba82728a0
@AmKuSaL <78016063+kusalCY@users.noreply.github.com>
2021-04-19 21:36:41 +0530

    Update __init__.py

--- CyberSongDL/__init__.py
+++ CyberSongDL/__init__.py
@@ -10,4 +10,4 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-Jebot = Client("CyberSongDL", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
+CHbot = Client("CyberSongDL", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
