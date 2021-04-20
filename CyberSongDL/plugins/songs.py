4d10d705db84a3c4a90bde4d947a71888f9694b7
@AmKuSaL <78016063+kusalCY@users.noreply.github.com>
2021-04-20 12:20:42 +0530

    Update songs.py

--- CyberSongDL/plugins/songs.py
+++ CyberSongDL/plugins/songs.py
@@ -54,7 +54,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '<b>Song Uploaded by @Cyber0Hacker</b>'
+        rep = '𝗦𝗼𝗻𝗴 𝗨𝗽𝗹𝗼𝗮𝗱𝗲𝗱 𝗯𝘆 @Cyber0Hacker'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
